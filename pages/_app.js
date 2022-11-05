63b463a51fde7336206c7788f49efd57da3b9cf1
sobebarali <sobebar.ali17@gmail.com>
2022-11-06 00:01:45 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,23 @@
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import  Router  from 'next/router'
+import ProgressBar from "@badrap/bar-of-progress"
+
+
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  text: "Loading",
+  strokeWidth: 2,
+  className: 'z-50',
+  delay: 100
+})
+
+Router.events.on("routeChangeStart", progress.start)
+Router.events.on("routeChangeComplete", progress.finish)
+Router.events.on("routeChangeError", progress.finish)
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/search.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useRouter } from "next/dist/client/router";
+import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
+import MapGL from "../components/MapGL";
+
+
+
+const Search = ({ searchResult }) => {
+  const router = useRouter();
+  const { location, startDate, endDate, guestNumber } = router.query;
+  const formatedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formatedEndDate = format(new Date(endDate), "dd MMMM yy");
+
+  const range = `${formatedStartDate} - ${formatedEndDate}`;
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${guestNumber} guest`} />
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ Stays - {range} - for {guestNumber} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
+
+          <div className="flex flex-col">
+          {searchResult.map(
+            ({ img, location, title, description, star, price, total }) => (
+              <InfoCard
+                key={img}
+                img={img}
+                location={location}
+                title={title}
+                description={description}
+                star={star}
+                price={price}
+                total={total}
+              />
+            )
+          )}
+          </div>
+        </section>
+        <section className="hidden xl:inline-flex xl:min-w-[600px]">
+            <MapGL searchResult={searchResult} />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResult = await fetch("https://www.jsonkeeper.com/b/5NPS").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+        searchResult,
+    },
+  };
+}
