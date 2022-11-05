420bbe38eb5c3562e2fa50f0ce80824bd4685e7f
sobebarali <sobebar.ali17@gmail.com>
2022-11-05 16:58:09 +0530

    refactor

--- components/Banner.js
+++ components/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
       <Image src="https://links.papareact.com/0fm" fill className="object-cover" alt="banner"/>
       <div className='absolute top-1/2 w-full text-center'>
         <p className='text-sm sm:text-lg '>Not sure where to go? Perfect.</p>
-        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm Flexible</button>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">Flexible</button>
       </div>
     </div>
   )
