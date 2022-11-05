67b0279ad56c6d035299eaa152279025eaa1ea8a
sobebarali <sobebar.ali17@gmail.com>
2022-11-06 00:00:01 +0530

    map api added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["links.papareact.com"]
+    domains: ["links.papareact.com", "upload.wikimedia.org","a0.muscache.com"]
+  },
+  env: {
+    mapbox_key: "pk.eyJ1Ijoic29iZWJhcmFsaSIsImEiOiJjbGE0Mmpjb3UwdjQwM3BtemluaGh4dWpsIn0.h2kE56kLM6wDmJv-H-E3nA"
   },
   reactStrictMode: true,
 }
